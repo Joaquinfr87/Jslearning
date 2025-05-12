2141c3fc16d632ca08e1491c8d809bd6c3964a13
Joaquinfr87 <vader87joaquin@gmail.com>
2025-05-12 12:42:05 -0400

    02

--- 01-values.js
+++ 01-values.js
@@ -28,3 +28,49 @@ console.log(!true); //not
 //operadores de comparacion
 console.log(10 == "10"); //igualdad (==) compara el valor, no el tipo de dato
 console.log(10 === "10"); //igualdad estricta (===) compara el valor y el tipo de dato
+
+//if corto
+console.log(true?1:2);//1
+
+console.log(false?1:2);//2
+//undefines y null son lo mismo
+
+console.log(8 * null)
+// → 0
+console.log("5" - 1)
+// → 4
+console.log("5" + 1)
+// → 51
+console.log("five" * 2)
+// → NaN
+console.log(false == 0)
+// → true
+
+console.log(null == undefined);
+// → true
+console.log(null == 0);
+// → false
+
+//operadores logicos
+//siempre va de izquierda a derecha 
+//si el primer valor es verdadero, devuelve el primer valor, si no, devuelve el segundo valor
+
+console.log(null || "user")
+// → user
+console.log("Agnes" || "user")
+// → Agnes
+
+//? hace lo mismo que | pero a la derecha
+
+console.log(0 || 100);
+// → 100
+console.log(0 ?? 100);
+// → 0
+
+//el && devolvera el primer valor falso y sis todos son verdaderos devolvera el ultimo valor
+console.log(0 && 100);
+// → 0
+console.log(100 && 0);
+// → 0
+console.log(100 && 200);
+// → 200
---
+++ 02-program-structure.js
@@ -0,0 +1,225 @@
+//expresiones
+1;
+!false;
+//enlaces o bidings
+let x=5;
+let y=4*5;
+
+//let y var son similares pero tienes usos de bloque diferentes
+
+//puedes usar var en funciones 
+//ES MEJOR USAR LETy var solo en algunos casos sabiendo el porque 
+//const es una constante
+
+
+console.log(x+y);//25
+//x=5, y=20
+//x es un enlace a 5, y es un enlace a 20
+
+x+=50; //suma 50 a x
+x++; //suma uno a x
+x--;//resta uno a x
+
+j=5,nano,nombre="Juan";
+
+//entradas de valor
+
+prompt("Ingresa tu nombre: ");
+//prompt es una funcion que pide un valor al usuario y lo devuelve como string
+// una funcion tambien es console.log
+
+//$ en string
+console.log(`Hola ${nombre}`);
+//template string, se usa para concatenar variables dentro de un string
+//se usa el simbolo $ para indicar que es una variable se usa otro tipo de comillas ``
+
+//CONTROL DE FLUJO O ESTRUCTURAS DE CONTROL
+//if, else, switch, for, while, do while, break, continue
+
+//if
+let theNumber = Number(prompt("Pick a number"));
+if (!Number.isNaN(theNumber)) {
+  console.log("Your number is the square root of " +
+              theNumber * theNumber);
+}
+
+if (1 + 1 == 2) console.log("It's true");
+// → It's true
+
+//if else
+theNumber = Number(prompt("Pick a number"));
+if (!Number.isNaN(theNumber)) {
+  console.log("Your number is the square root of " +
+              theNumber * theNumber);
+} else {
+  console.log("Hey. Why didn't you give me a number?");
+}
+//if cadena
+let num = Number(prompt("Pick a number"));
+
+if (num < 10) {
+  console.log("Small");
+} else if (num < 100) {
+  console.log("Medium");
+} else {
+  console.log("Large");
+}
+
+//while y do loops
+
+let i = 0;
+
+while (i < 5) {
+  console.log("Valor de i en while:", i);
+  i++;
+}
+
+let j = 0;
+
+do {
+  console.log("Valor de j en do...while:", j);
+  j++;
+} while (j < 5);
+
+//for loops
+for (let number = 0; number <= 12; number = number + 2) {
+    console.log(number);
+  }
+
+//romper un loop
+for (let current = 20; ; current = current + 1) {
+    if (current % 7 == 0) {
+      console.log(current);
+      break;
+    }
+  }
+
+  //switch
+  switch (prompt("What is the weather like?")) {
+    case "rainy":
+      console.log("Remember to bring an umbrella.");
+      break;
+    case "sunny":
+      console.log("Dress lightly.");
+    case "cloudy":
+      console.log("Go outside.");
+      break;
+    default:
+      console.log("Unknown weather type!");
+      break;
+  }
+
+
+/*Ejercicios de loops*/
+/*Nivel intermedio (1–7)
+
+    Cuenta cuántos números del 1 al 100 son divisibles entre 3.
+
+    Dado un array de nombres, imprímelos todos con un for.
+
+    Cuenta cuántas letras "a" hay en una cadena de texto.
+
+    Suma todos los valores de un array numérico.
+
+    Invierte un array sin usar .reverse().
+
+    Imprime solo los caracteres de una cadena que no sean vocales.
+
+    Pide números hasta que el usuario escriba "0", luego muestra la suma total (do...while).
+*/
+for(let i=0;i<100;i++){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+
+let name = prompt("Ingresa tu nombre: ");
+for(let i=0;i<name.length;i++){
+    console.log(name[i]);
+}
+
+
+
+name = prompt("Ingresa una oracion: "),c=0;
+
+for(let i =0;i<name.length;i++){
+    if(name[i]=='a'){
+        console.log(name[i]);c++;}
+}
+console.log(c);
+
+let nums = [1,2,3,4,5,6,7,8,9,10],s=0;
+for(let i=0;i<nums.length;i++){
+    s+=nums[i];
+}
+console.log(s);
+
+
+for(let i=0;i<=nums.length;i++){
+    if(i<nums.length/2){
+        let aux=nums[i];
+        nums[i]=nums[nums.length-i-1];
+        nums[nums.length-i-1]=aux;
+    }
+}
+console.log(nums);
+
+let cadena = prompt("Ingresa una cadena: "),vocales = ['a','e','i','o','u'];
+for(let i=0;i<cadena.length;i++){
+    if(!vocales.includes(cadena[i])){
+        console.log(cadena[i]);
+    }
+}
+
+let sum=0,number;
+do{
+
+    number=prompt("Ingresa un numero: ");
+    sum+=Number(number);
+    
+}while(number!=0);
+console.log(sum);
+/*    Verifica si un número es primo usando un bucle for.
+
+    Genera los primeros n números de la serie de Fibonacci.
+
+    Crea una pirámide de asteriscos con altura n:
+
+*
+**
+***
+****
+
+    Simula una calculadora que siga funcionando hasta que el usuario escriba "salir" (do...while).
+
+    Calcula el factorial de un número n con un bucle (n!).
+
+    Busca el número más grande en un array sin usar métodos de array.
+
+    Verifica si una palabra es un palíndromo (se lee igual al revés).*/
+
+let numero=prompt("Ingresa un numero: ");
+let EsPrimo=true;
+let raiz=Math.sqrt(numero);
+
+for(let i=2;i<=raiz;i++){
+  if(numero%i==0){
+    EsPrimo=false;
+    break;
+  }
+}
+console.log(EsPrimo?"Es primo":"No es primo");
+
+let n = prompt("Ingrese la altura n");
+
+let chain="";
+for(let i=0;i<n;i++){
+  chain+="*";
+  console.log(chain); 
+}
+//Otra alternativa mucho mejor
+for(let line="#";line.length<=n;line+="#"){
+    console.log(line);
+  }
+
+
