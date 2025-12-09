a98f83509ff670f8dad11354280289576297320e
Joaquin <vader87joaquin@gmail.com>
2025-12-09 11:39:05 -0400

    avances clases

--- UPDS/tw/js-repaso.js
+++ UPDS/tw/js-repaso.js
@@ -10,15 +10,59 @@ const factorial = (a)=>{
 }
 console.log(factorial(5))
 
+console.log(estudiante.aprobado())
 let estudiante = {
-  notas:{
-    primer: 100,
-    segundo: 99,
-    final:67
+  nombre: "Joaquin",
+  apellido: "Felipez",
+  notas: [
+    { materia: "Programacion", parcial: "Primer parcial", nota: 100 },
+    {materia: "Algoritmos",parcial: "Segundo parcial", nota:79},
+    {materia: "Programacion", parcial:"Segundo parcial",nota: 99}
+  ],
+  telefono: [45651312,23234525,76756756],
+  agregarTelefono(numero){
+    this.telefono.push(numero)
   },
-  aprobado(){
-    let notas = Object.values(this.notas)
-    return notas.reduce((a,b)=>a+b)/notas.length
+  get estado (){
+    let notaTotal = this.notas.reduce((a,b)=> b.nota + a)
+    if(notaTotal/this.notas.length>50)return true
+    return false
+  }
+};
+
+
+
+
+
+class Persona{
+  constructor(nombre,apellido,fecha_nac){
+    this.nombre=nombre
+    this.apellido=apellido
+    this.fecha_nac=fecha_nac
+  }
+  get edad(){
+  let fecha = new Date();
+    return fecha.getFullYear()-fecha_nac.getFullYear
+  }
+  mostrar(){
+    console.log(`nombre: ${this.nombre}\n
+    apellido: ${this.apellido}\nombre
+    edad: ${this.edad}`)
   }
 }
-console.log(estudiante.aprobado())
+class Estudiante extends Persona{
+  
+  constructor(nombre,apellido,fecha_nac,carrera){
+    this.nombre=nombre
+    this.apellido=apellido
+    this.fecha_nac=fecha_nac
+    this.carrera=carrera
+  }
+
+  mostrar(){
+    console.log(`nombre: ${this.nombre}\n
+    apellido: ${this.apellido}\nombre
+    edad: ${this.edad}\n 
+    carrera: ${this.carrera}`)
+  }
+}
