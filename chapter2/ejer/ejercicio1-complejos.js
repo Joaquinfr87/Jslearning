22533ea7c75f619a5f04be970c59550b1323953b
joaquinfr87 <vader87joaquin@gmail.com>
2025-12-10 19:48:22 -0400

    errors advanced

--- chapter2/ejer/ejercicio1-complejos.js
+++ chapter2/ejer/ejercicio1-complejos.js
@@ -1,1 +1,78 @@
-let numero
+//1 
+let par=0;
+let impar=0;
+for(let numero = Number(prompt("Ingresa un numero"));numero>0;numero= numero%Math.pow(10, Math.floor(Math.log10(numero)))){
+  if(Math.floor(numero/Math.pow(10, Math.floor(Math.log10(numero))))%2==0) par++;
+  else impar++
+}
+
+console.log("par= "+par);
+console.log("impar= "+impar);
+//2
+let num = Number(prompt("Ingresa un numero para la piramide"))
+for(let i=0;i<num;i++){
+  let text=""
+  for(let j=num-i;j>0;j--){
+    text+=(num - j+1) + " "
+  }
+  console.log(text)
+}
+// 1 2 3 4 5 6 7 
+// 2 3 4 5 6 7 
+// 3 4 5 6 7 
+// 4 5 6 7 
+// 5 6 7 
+// 6 7 
+// 7 
+for(let i=0;i<num;i++){
+  let text=""
+  for(let j=num-i;j>0;j--){
+    text+=(num - j-i+1) + " "
+  }
+  console.log(text)
+}
+// 1 2 3 4 5 6 7 
+// 1 2 3 4 5 6 
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
+
+
+
+//3
+let nums = [];
+for(let numero = Number(prompt("Ingresa un numero de Armstrong a verificar"));numero>0;numero= numero%Math.pow(10, Math.floor(Math.log10(numero)))){
+  nums.push(Math.floor(numero/Math.pow(10,Math.floor(Math.log10(numero)))))
+}
+let suma = 0;
+for(let e of nums){
+  suma+= Math.pow(e,nums.length)
+}
+if(Number(nums.join("")==suma)){
+  console.log("Es un numero Armstrong")
+}
+else{ console.log("No es un numero Armstrong")}
+//4
+let segundos = prompt("Ingrese los segundos");
+let sec = segundos%60;
+let min = ((segundos-sec)/60)%60;
+let horas = ((((segundos-sec)/60)-min)/60)%24
+let dias = (((((segundos-sec)/60)-min)/60)-horas)/24
+
+
+console.log(`${dias} dias ${horas} horas ${min} min ${sec}`)
+//5
+let fib1=0
+let fib2=1
+let parFib=1
+let imparFib=1
+while(fib2<10000){
+  let aux = fib2;
+  fib2+=fib1;
+  fib1=aux;
+  console.log(fib2);
+  (fib2%2)?parFib++:imparFib++
+} 
+//6
---
+++ chapter8/error.js
@@ -0,0 +1,60 @@
+// Al ser Javascript un lenguajes tan abierto y flexible da la contra
+// de que cosas que no deberian suceder, suceden lo que pueden ocasionar 
+// errores o bugs no deseables en nuestro codigo que son dificiles de encontrar
+// o incluso dificiles de diagnosticar
+// essos bugs pueden nunca manifestarse y correr por todo nuestro codigo dandonos
+// una salida erronea que sera muy dificl de detectar
+
+//Strict mode
+//el modo stricto te ayuda a mantener una mejor sintaxis en tu codigo
+//son alugnos aspectos que Javascript paso por alto a veces, y rellena los espacios 
+//de codigo pero en strict mode esto ya no sucedera por lo que tendras un condigo
+//mas stricto en su sintaxis y declaracion ayudandote a evitar errores
+
+function Person(name) { this.name = name; }
+let ferdinand = Person("Ferdinand"); // oops
+console.log(name);
+// → Ferdinand
+
+"use strict";
+function Person(name) { this.name = name; }
+let ferdinand = Person("Ferdinand"); // forgot new
+// → TypeError: Cannot set property 'name' of undefined
+
+//esta mal
+function Persona(nombre){
+  this.nombre = nombre
+}
+let franz = Persona("Franz")
+console.log(nombre)
+
+//esta bien
+function Persona(nombre){
+  this.nombre = nombre
+}
+let franz =new Persona("Franz")
+console.log(franz.nombre)
+
+
+//Types
+//Javascript es un lenguaje muy abierto y nada estricto con los typos
+//esto resulta en muchos errores, tanto de confusion como de bugs
+//por eso es siempre una buena idea agregar un comentario sobre los tipos 
+//o usar un dialecto de Javascript como Typescript
+
+
+//Test 
+//Es util escribir una funcion test que comprueba la efectividad de nuestro codigo
+//para poder evitar errores
+function test(label, body) {
+  if (!body()) console.log(`Failed: ${label}`);
+}
+test("convert Latin text to uppercase", () => {
+  return "hello".toUpperCase() == "HELLO";
+});
+test("Verificar que cuadrado de 5 sea 25", () => {
+  return cuadrado(5) == 25;
+});
+
+
+
